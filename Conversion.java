5a4d73b0eb09f8c5f6471d7e9cfa039f8f0f5a21
Alizahid11 <zahid_sakhizada@yahoo.com>
2019-04-15 14:52:15 +0100

    first commit

---
+++ Conversion.java
@@ -0,0 +1,43 @@
+class Convert{
+		public String sick(int n){
+
+	int a;
+	String x = " ";
+
+			
+			while (n>0){
+			a = n % 2;
+			
+			
+					x = a + "" + x;
+					n = n/2;
+				}
+				return x;
+	}
+		public float hmmm(String x){
+
+	int n; 
+	float a;
+	n=0;
+	a=0;
+	
+
+		for (int i =x.length(); i>0; i--,n++){
+					a+= Integer.parseInt(x.substring(i-1,i)) * Math.pow(2,n);
+
+				}
+
+					return a;
+}
+}
+
+class Conversion{
+	public static void main(String az[]){
+
+		Convert z = new Convert();
+		
+		System.out.println("Binary number:" + z.sick(25));
+		System.out.println("Decimal number: " + z.hmmm("11001"));
+
+	}
+}
---
+++ Convert.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Convert{
+
+
+	public static void main(String az[]){
+
+	int n, count = 0,a;
+	String x = " ";
+	Scanner s = new Scanner(System.in);
+	System.out.print ("Enter any decimal number:");
+
+
+	
+		
+		
+			n = s.nextInt();
+			while (n>0){
+			a = n % 2;
+			
+			
+					x = a + "" + x;
+					n = n/2;
+		}
+		System.out.println("Binary number:"+x);
+
+		System.out.println("No. of is:"+count);
+	}
+
+}
